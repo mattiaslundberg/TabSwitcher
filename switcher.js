55011f1b8a5afb5905f5bb9195d1174be62e9a84
Mattias Lundberg <lundberg.mattias@gmail.com>
2013-10-17 20:23:42 +0200

    Support for keyboard navigation.

--- switcher.js
+++ switcher.js
@@ -19,6 +19,8 @@ function update_list(/* String */ search) {
 			row.style["display"] = "";
 		} else {
 			row.style["display"] = "none";
+			if (row.classList.contains("selected"))
+				move_selection_down();
 		}
 	};
 }
@@ -28,7 +30,7 @@ function on_search_change(event) {
 		var table = document.getElementById("current-hits");
 		for (var i = 0; i < table.children.length; i++) {
 			row = table.children[i];
-			if (row.style["display"] !== "none") {
+			if (row.classList.contains("selected")) {
 				row.firstChild.firstChild.click();
 				return;
 			}
@@ -47,13 +49,67 @@ function add_tab_to_list(/* Tab */ tab) {
 	var td = document.createElement("td");
 	var a = document.createElement("a");
 	a.href = "#";
+	a.tabIndex = -1;
 	a.onclick = function() { open_tab(tab); };
 	a.tab = tab;
 	a.innerHTML = tab.title;
+	
+	if (table.children.length === 0)
+		tr.classList.add("selected");
+	
 	td.appendChild(a);
 	tr.appendChild(td);
-	
 	table.appendChild(tr);
+}
+
+function move_selection_up() {
+	var table = document.getElementById("current-hits");
+	var moved = false;
+	
+	for (var i = table.children.length - 1; i >= 0 ; --i) {
+		row = table.children[i];
+		if (!moved && row.classList.contains("selected")) {
+			row.classList.remove("selected");
+			moved = true;
+			continue;
+		}
+		if (moved && row.style["display"] !== "none") {
+			row.classList.add("selected");
+			return;
+		}
+	};
+	for (var i = table.children.length - 1; i >= 0 ; --i) {
+		row = table.children[i];
+		if (row.style["display"] !== "none") {
+			row.classList.add("selected");
+			return;
+		}
+	};
+}
+
+function move_selection_down() {
+	var table = document.getElementById("current-hits");
+	var moved = false;
+	
+	for (var i = 0; i < table.children.length; i++) {
+		row = table.children[i];
+		if (!moved && row.classList.contains("selected")) {
+			row.classList.remove("selected");
+			moved = true;
+			continue;
+		}
+		if (moved && row.style["display"] !== "none") {
+			row.classList.add("selected");
+			return;
+		}
+	};
+	for (var i = 0; i < table.children.length; i++) {
+		row = table.children[i];
+		if (row.style["display"] !== "none") {
+			row.classList.add("selected");
+			return;
+		}
+	};
 }
 
 chrome.tabs.query({}, function(tabs) {
@@ -70,8 +126,15 @@ chrome.tabs.query({}, function(tabs) {
 document.body.onkeyup = function(event) {
 	if (event.keyCode === 27) // ESC
 		window.close();
-	// TODO: Navigate tabs with arrow keys.
-}
+	if (event.keyCode === 38)
+		move_selection_up();
+	if (event.keyCode === 40)
+		move_selection_down();
+	if (!event.shiftKey && event.keyCode === 9)
+		move_selection_down(); // TODO: Prevent default
+	if (event.shiftKey && event.keyCode === 9)
+		move_selection_up(); // TODO: Prevent default
+};
 
 document.querySelector("#search").addEventListener("keypress", on_search_change);
 document.getElementById("search").focus();
